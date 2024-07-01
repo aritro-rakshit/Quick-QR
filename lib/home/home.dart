baacdee18efb0eff26c5ee958321e2de3140a555
“Aritro <aritro@tradelab.in>
2024-07-01 13:16:58 +0530

    initial commit

---
+++ lib/home/home.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+ late ScrollController _scrollController;
+
+  @override
+  void initState() {
+    _scrollController = ScrollController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(elevation: 0,title:const Text("Quick QR"),) ,
+        body: SingleChildScrollView(
+          controller: _scrollController,
+          child: Column(
+
+          ),
+        ),
+      ),
+    );
+  }
+}
